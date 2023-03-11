fa40eb24706f2911a4cf3f680357be9a072af05b
Bernard Calma <nard.calma@gmail.com>
2023-03-11 01:35:51 -0800

    added dotenv

--- nexchaptersBackend/nexchaptersBackend/settings.py
+++ nexchaptersBackend/nexchaptersBackend/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'nexchaptersBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'manga',
+        'USER': 'postgres',
+        'PASSWORD': 'Aluminus10!',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
     }
 }
 
