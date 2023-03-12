d81c55c9e4e426ae840f2f6f5cca2abf131f119d
Bernard Calma <nard.calma@gmail.com>
2023-03-12 01:26:23 -0800

    added link in models

--- nexchaptersBackend/manga/models.py
+++ nexchaptersBackend/manga/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Manga(models.Model):
     title= models.CharField(max_length=50)
     image= models.CharField(max_length=200)
+    link = models.CharField(max_length=200, default="")
     total_chapters= models.IntegerField()
     current_chapter= models.IntegerField() 
