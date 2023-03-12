6ee15db0b6aaa03b4424c65e708a8967c55150ac
Bernard Calma <nard.calma@gmail.com>
2023-03-11 18:37:40 -0800

    modified manga model

--- nexchaptersBackend/manga/models.py
+++ nexchaptersBackend/manga/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Manga(models.Model):
-    title: models.CharField()
-    image: models.CharField()
+    title: models.CharField(max_length=200, null=True)
+    image: models.CharField(max_length=200, null=True)
     total_chapters: models.IntegerField()
     current_chapter: models.IntegerField()
+
+    def __str__(self):
+        return self.title
