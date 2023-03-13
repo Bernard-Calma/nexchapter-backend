5bfa51dd84e81b9ac887192a8da9749e68e4dc1d
Bernard Calma <nard.calma@gmail.com>
2023-03-13 16:49:58 -0700

    added default value for manga model

--- nexchaptersBackend/manga/models.py
+++ nexchaptersBackend/manga/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Manga(models.Model):
-    title= models.CharField(max_length=50)
-    image= models.CharField(max_length=200)
+    title= models.CharField(max_length=50, null=False)
+    image= models.CharField(max_length=200, default="")
     link = models.CharField(max_length=200, default="")
     total_chapters= models.IntegerField()
-    current_chapter= models.IntegerField() 
+    current_chapter= models.IntegerField(default=0) 
--- nexchaptersBackend/manga/views.py
+++ nexchaptersBackend/manga/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 # Return JSON File
 from django.core import serializers
+import json
 from django.http import JsonResponse
+from django.http import HttpResponse
 # Create your views here.
 
 # Model
@@ -9,3 +12,22 @@ from manga.models import Manga
 def index(request):
     data = list(Manga.objects.values())
     return JsonResponse({'mangaList': data}, safe=False)
+
+@csrf_exempt
+def add(request):
+    print("Post API Called")
+    newManga = Manga(
+        title= request.body.title,
+        link = request.body.link,
+        image= request.body.image,
+        current_chapter = request.body.currentChapter
+    )
+    newManga.save()
+    return JsonResponse({
+        'status': 200,
+        'message': "New manga successfully added.",
+        'added': newManga,
+        })
+    # data = json.loads(request.body) 
+    # print(data)
+    # return HttpResponse(data['message']) 
