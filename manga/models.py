bd4c10d0a474c995e8d95cd972b189ab132ba164
Bernard Calma <nard.calma@gmail.com>
2023-03-19 15:23:40 -0700

    added settings and procfile

--- manga/models.py
+++ manga/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Manga(models.Model):
-    title= models.CharField(max_length=50, null=False)
+    title= models.CharField(max_length=100, null=False)
     image= models.CharField(max_length=200, default="")
     link = models.CharField(max_length=200, default="")
     total_chapters= models.IntegerField(default=0)
